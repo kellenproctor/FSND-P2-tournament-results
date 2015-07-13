b2a37f3104120e6843e977b983dd7769e8c40c66
Kellen Proctor <kellenproctor@gmail.com>
2015-07-13 10:56:53 +0800

    FEAT: Add reportMatch function

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -82,7 +82,11 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
+    DB = connect()
+    c = DB.cursor()
+    c.execute("INSERT INTO match values (%s, %s);", (winner, loser))
+    DB.commit()
+    DB.close()
  
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
