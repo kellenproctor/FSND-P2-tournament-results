8ad3351b1bcb1aa4e4050edc3b4fd6306ed1f82c
Kellen Proctor <kellenproctor@gmail.com>
2015-07-13 10:34:39 +0800

    FEAT: create database schema in tournament.sql file, including views for win counts and match counts

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,34 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE DATABASE tournament;
 
+\c tournament;
+
+-- Player table with columns id, and name
+CREATE TABLE player (
+    id serial primary key,
+    name text
+);
+
+-- Match table with winner_id, and loser_id
+CREATE TABLE match (
+    winner_id integer references player (id),
+    loser_id integer references player (id)
+);
+
+-- Win_count View which creates a table with id, and win count from joining
+-- the player and match table
+CREATE VIEW win_count as 
+    SELECT id, count(winner_id) as wins
+    FROM player LEFT JOIN match
+    ON id = winner_id
+    GROUP BY id;
+
+-- Match_count View which creates a table with id, and match count from
+-- joining the player and match table
+CREATE VIEW match_count as
+    SELECT id, count(winner_id+loser_id) as matches_played
+    FROM player LEFT JOIN match
+    ON id = winner_id OR id = loser_id
+    GROUP BY id;
